ed5e4521baa5fab778c07204640356540f574031
Inese24 <germaneinese@gmail.com>
2024-06-01 19:09:30 +0300

    Nodarbiba_3b

---
+++ Nodarbiba_03/JS/first_script_1.js
@@ -0,0 +1,27 @@
+a = 10
+console.log("a mainīgā vērtība: " +a);
+
+var x = 10;
+console.log("x mainīgā vērtība pirms {}: " +x);
+
+
+
+
+//https://kas.id.lv/2022/12/21/javascript-mainigie-variables/
+
+var vārds = "Jānis";
+
+
+var uzvārds = "Bērzs";
+
+
+var vecums = "30";
+
+
+//izvadam rezultātu
+
+
+document.write(vārds + " " + uzvārds + "<br>");
+
+
+document.write(vecums);
