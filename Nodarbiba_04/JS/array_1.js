0d123e7cafc0536b0e305873fbc459393acbfc03
Inese24 <germaneinese@gmail.com>
2024-06-05 23:09:48 +0300

    Nodarbiba_03_labojumi

---
+++ Nodarbiba_04/JS/array_1.js
@@ -0,0 +1,11 @@
+console.log (new Date())
+
+/*
+const cars1 = "Saab";
+const cars1 = "Volvo";
+const cars1 = "BMW";
+document.getElementById("demo").innerHTML = cars1+', '+cars2+', '+cars3+';
+*/
+
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
