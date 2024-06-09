45af5b1a0a14ac80618ea7ce29c40659b515ada6
Inese24 <germaneinese@gmail.com>
2024-06-09 21:16:32 +0300

    Novdarbiba_04b

--- Nodarbiba_04/JS/array_1.js
+++ Nodarbiba_04/JS/array_1.js
@@ -1,10 +1,16 @@
+/*
+laika zīmogs, laika tipa mainīgā izmantošana - drukāt aktuālo datumu
+*/
 console.log (new Date())
 
 /*
 const cars1 = "Saab";
-const cars1 = "Volvo";
-const cars1 = "BMW";
-document.getElementById("demo").innerHTML = cars1+', '+cars2+', '+cars3+';
+const cars2 = "Volvo";
+const cars3 = "BMW";
+document.getElementById("demo").innerHTML = cars1+','+cars2+','+cars3;
+*/
+
+/* nodefinēts masīvs aar identifikatoru cars; atribūts = demo
 */
 
 const cars = ["Saab", "Volvo", "BMW"];
