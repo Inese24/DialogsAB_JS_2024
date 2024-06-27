d52405e4922e879cf7aeb17a4ad1c32b6d18ece6
Inese24 <germaneinese@gmail.com>
2024-06-27 23:16:02 +0300

    Nodarbiba_11b

--- Nodarbiba_11/JS/script.js
+++ Nodarbiba_11/JS/script.js
@@ -1,14 +1,3 @@
 <! https://www.javascripttutorial.net/javascript-dom/javascript-getelementbyid/
 
 const element = document.getElementById("demo");
-
-
-DIALOGS AB
-18:50
-document.childNodes
-
-document.childNodes[0]
-document.childNodes[1]
-document.childNodes[2]
-
-document.childNodes[2].childNodes[1].childNodes
